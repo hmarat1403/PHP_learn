70124e001750b0824a7383c04a31a6335f071cd0
hmarat1403 <hmarat1403@gmail.com>
2024-05-18 13:34:59 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>    
+    <?php
+    echo 'Hello world!';
+
+    echo "<h1>Test PHP with tag</h1>";
+
+    print("<p>Test PHP with tag in print command</p>");
+
+    $box = "Box";
+    echo "<br>".$box;
+    $test = "<p>Ntst sdjjfhsdjhf </p>";
+    $link = "<a href='https://yandex.ru'>Test Link to yandex</a>";
+    echo $test."<br>".$link;
+    ?>
+</body>
+</html>
